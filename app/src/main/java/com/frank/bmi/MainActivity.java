d441a56c67bbd441e8438071d5139a5ee3ffb819
purplesunset871019 <frank871019>
2018-10-02 16:33:04 +0800

    Log.d,findViewByid

--- app/src/main/java/com/frank/bmi/MainActivity.java
+++ app/src/main/java/com/frank/bmi/MainActivity.java
@@ -2,6 +2,9 @@ package com.frank.bmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void bmi(View view){
+        Log.d("MainActivity" ,"bmi");
+        EditText edweight = findViewById(R.id.ed_weight);
+        EditText edheight = findViewById(R.id.ed_height);
+        String w = edweight.getText().toString();
+        String h = edheight.getText().toString();
+        Log.d("MainActivity",w+"/"+h);
+        float weight = Float.parseFloat(w);
+        float height = Float.parseFloat(h);
+        float bmi = weight/(height*height);
+    }
+
+
 }
