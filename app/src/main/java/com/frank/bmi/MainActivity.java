167d58d9dd141be25e4dd3b7a14cbddd97d10c92
purplesunset871019 <frank871019>
2018-10-09 16:27:29 +0800

    BMI 2(new AlertDialog、setMessage、setTittle)

--- app/src/main/java/com/frank/bmi/MainActivity.java
+++ app/src/main/java/com/frank/bmi/MainActivity.java
@@ -1,29 +1,49 @@
 package com.frank.bmi;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edweight;
+    private EditText edheight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edweight = findViewById(R.id.ed_weight);
+        edheight = findViewById(R.id.ed_height);
     }
 
     public void bmi(View view){
         Log.d("MainActivity" ,"bmi");
-        EditText edweight = findViewById(R.id.ed_weight);
-        EditText edheight = findViewById(R.id.ed_height);
+
         String w = edweight.getText().toString();
         String h = edheight.getText().toString();
         Log.d("MainActivity",w+"/"+h);
         float weight = Float.parseFloat(w);
         float height = Float.parseFloat(h);
         float bmi = weight/(height*height);
+        Log.d("MainActivity",bmi+"");
+        Toast.makeText(this,"your bmi is"+bmi,Toast.LENGTH_LONG).show();
+        new AlertDialog.Builder(this)
+                .setMessage("Your BMI is"+bmi)
+                .setTitle("BMI Coculate")
+                .setPositiveButton("Finish", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        edheight.setText("");
+                        edweight.setText("");
+                    }
+                })
+                .show();
     }
 
 
