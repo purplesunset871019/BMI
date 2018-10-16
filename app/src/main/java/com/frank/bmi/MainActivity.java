19b168c24faf4353100cd670091dca19b80b22ac
purplesunset871019 <hsb41202@gmail.com>
2018-10-16 16:40:12 +0800

    BMI 3 字串提出、語言翻譯、新的新增視窗方法

--- app/src/main/java/com/frank/bmi/MainActivity.java
+++ app/src/main/java/com/frank/bmi/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText edweight;
     private EditText edheight;
+    private Button help;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         edweight = findViewById(R.id.ed_weight);
         edheight = findViewById(R.id.ed_height);
+        help = findViewById(R.id.bt_help);
+        findview();
+    }
+
+    public void findview(){
+        help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle(R.string.helpTittle)
+                        .setMessage(R.string.helpMessage)
+                        .setPositiveButton(R.string.helpBack, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                    }
+                                }
+                        )
+                        .show();
+            }
+        });
     }
 
     public void bmi(View view){
@@ -35,9 +57,9 @@ public class MainActivity extends AppCompatActivity {
         Log.d("MainActivity",bmi+"");
         Toast.makeText(this,"your bmi is"+bmi,Toast.LENGTH_LONG).show();
         new AlertDialog.Builder(this)
-                .setMessage("Your BMI is"+bmi)
-                .setTitle("BMI Coculate")
-                .setPositiveButton("Finish", new DialogInterface.OnClickListener() {
+                .setMessage(getString(R.string.submitMessage)+bmi)
+                .setTitle(R.string.submitTittle)
+                .setPositiveButton(R.string.submitBack, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         edheight.setText("");
@@ -49,13 +71,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void help(View view){
-        Log.d("MainActivity","help");
+    public void support(View view){
+        Log.d("MainActivity","support");
         Toast.makeText(this,"Weight(kg)",Toast.LENGTH_LONG).show();
         new AlertDialog.Builder(this)
-                .setTitle("Help").setMessage("Weight(kg)"+"\n"+"Height(m)")
-                //.setMessage("Height(m)")
-                .setPositiveButton("back", new DialogInterface.OnClickListener() {
+                .setTitle(R.string.supportTittle).setMessage("Weight(kg)"+"\n"+"Height(m)")
+                .setPositiveButton(R.string.supportBack, new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 edheight.setText("");
